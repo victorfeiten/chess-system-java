7206f5d1f7846df9f1eaf343958466ad48e1d898
Victor Feiten <victorffeiten@gmail.com>
2023-07-24 09:14:31 -0300

    Implementing possible moves of Rook.

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,13 +28,11 @@ public class Program {
 				ChessPosition target = UI.readChessPosition(sc);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-				
-			}
-			catch(ChessException e) {
+
+			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
-			}
-			catch(InputMismatchException e) {
+			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -4,13 +4,13 @@ import boardgame.Board;
 import chess.ChessPiece;
 import chess.Color;
 
-public class King extends ChessPiece{
+public class King extends ChessPiece {
 
 	public King(Board board, Color color) {
 		super(board, color);
 
 	}
-	
+
 	@Override
 	public String toString() {
 		return "K";
@@ -18,7 +18,7 @@ public class King extends ChessPiece{
 
 	@Override
 	public boolean[][] possibleMoves() {
-		boolean [][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		return mat;
 	}
 }
